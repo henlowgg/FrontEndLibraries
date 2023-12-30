49bce0629e486b465a4e2b1053c80db017877c35
Jack Ault <henlowgg@outlook.com>
2023-12-30 12:41:28 -0500

    solution

---
+++ React/2. Create a Complex JSX Element/ComplexJSX.jsx
@@ -0,0 +1,30 @@
+const JSX = <div>
+    <h1>Heading.</h1>
+    <p>Paragraph</p>
+    <ul>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+    </ul>
+</div>
+
+
+
+/* 
+
+Nested JSX must return a single, top level element
+
+The solution has a parent div containing an h1, a p, and an unordered list (ul) that contains three li items
+
+Solutions
+const JSX = <div>
+  <h1>Heading.</h1>
+  <p>Paragraph</p>
+ <ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+</div>;  
+
+*/
