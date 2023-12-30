ecb316a0e716ded839dc82d87f379bdb62bcc83c
Jack Ault <henlowgg@outlook.com>
2023-12-30 13:05:37 -0500

    solution

--- React/3. Add Comments in JSX/AddComments.jsx
+++ React/3. Add Comments in JSX/AddComments.jsx
@@ -1,6 +1,9 @@
 const JSX = (
     <div>
       <h1>This is a block of JSX</h1>
+      {/*  */}
       <p>Here's a subtitle</p>
     </div>
   );
+
+  // need to add the comment method, either with a shortcut, but for clarity its just {/* */}
