dfe5402ff10a3cfaefc2c031f60121542e423856
Jack Ault <henlowgg@outlook.com>
2023-12-30 14:41:27 -0500

    comments

--- React/Pass Props to a Stateless Functional Component/PassProps.jsx
+++ React/Pass Props to a Stateless Functional Component/PassProps.jsx
@@ -23,3 +23,8 @@ const CurrentDate = (props) => {
       );
     }
   };
+
+// pass in props.date in the CurrentDate function
+// in the constructor, you need to state date={Date()}
+
+// It is standard to call this value props and when dealing with stateless functional components, you basically consider it as an argument to a function which returns JSX. You can access the value of the argument in the function body
