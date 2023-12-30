38bc6d24110769bd6810a73fc924c5b3631f4b81
Jack Ault <henlowgg@outlook.com>
2023-12-30 13:17:35 -0500

    solution

---
+++ React/7. Create a Stateless Functional Component/Stateless.jsx
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+    // Change code below this line
+  return (
+    <div>Some Text</div>
+  );
+  
+  
+    // Change code above this line
+  };
+
+  // add div and return div within the MyComponent function, (function name should be capitalized )
