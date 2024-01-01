b1456b599fb0929e86512a6c3b939cf8ff326c2e
Jack Ault <henlowgg@outlook.com>
2023-12-31 21:28:57 -0500

    comment for clarity

--- React/23. Render State in the User Interface Another Way/RenderStateInUI.jsx
+++ React/23. Render State in the User Interface Another Way/RenderStateInUI.jsx
@@ -18,3 +18,13 @@ class MyComponent extends React.Component {
       );
     }
   };
+
+/* 
+to render the state before returning it, declare the variable name 
+const name = this.state.name;
+then return the h1 holding name in it within curly brackets
+
+<h1>{name}</h1>
+
+just makes it so that name will now be able to access "this.state.name" because thats now the value of "name"
+*/
